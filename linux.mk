359365c49b2a996b06177cca5b352a0a1a5f9b1c
zinface <zinface@163.com>
2023-02-17 02:00:13 +0800

    repo: 独立 Linux 平台的 Uos 构建

--- linux.mk
+++ linux.mk
@@ -2,6 +2,7 @@ Msg   := 'Build with the following configuration:'
 One   := '1. make -f linux-universal.mk'
 Two   := '2. make -f linux-debian.mk package'
 Three := '3. make -f linux-appimage.mk package'
+Four  := '4. make -f linux-uos.mk package'
 
 
 all:
@@ -9,4 +10,5 @@ all:
 	@echo $(One)    "\n\t默认的通用 Linux 平台构建."
 	@echo $(Two)    "\n\t通用 Linux 平台的 Debian deb 构建."
 	@echo $(Three)  "\n\t通用 Linux 平台的 Appimage 构建."
+	@echo $(Four)   "\n\t独立 Linux 平台的 Uos 构建."
 
