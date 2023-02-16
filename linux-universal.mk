b2bdd382a1b9f48abc6678a2fa5d8dd4c19d3624
zinface <zinface@163.com>
2023-02-16 23:41:58 +0800

    repo: 通用 Linux 平台构建，一切的起始

---
+++ linux-universal.mk
@@ -0,0 +1,21 @@
+# Linux Universla - 通用 Linux 平台 构建方案
+
+CPUS=$(shell nproc)
+
+builddir  := build
+sourcedir := .
+CMAKE_DEBUG     := -DCMAKE_BUILD_TYPE=Debug
+CMAKE_RELEASE   := -DCMAKE_BUILD_TYPE=Release
+CMAKE_OPTIONS   := -DUSE_LINUX_UNIVERSAL=ON
+
+
+linux-universal:
+	cmake -B$(builddir) $(CMAKE_OPTIONS) $(CMAKE_DEBUG)
+	cmake --build build -- -j$(CPUS)
+
+
+linux-universal-release:
+	cmake -B$(builddir) $(CMAKE_OPTIONS) $(CMAKE_RELEASE)
+	cmake --build $(builddir) -- -j$(CPUS)
+
+# 此配置为构建 linux 通用版本构建
--- linux.mk
+++ linux.mk
@@ -1,4 +1,8 @@
 Msg   := 'Build with the following configuration:'
+One   := '1. make -f linux-universal.mk'
+
 
 all:
 	@echo $(Msg)
+	@echo $(One)    "\n\t默认的通用 Linux 平台构建."
+
