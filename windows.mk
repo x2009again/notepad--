4432551a1ed41b14ad8c17e0ba081f0c9dcee73b
zinface <zinface@163.com>
2023-02-19 02:34:27 +0800

    repo: 通用 Windows 平台构建，一切的起始

--- windows.mk
+++ windows.mk
@@ -1,4 +1,6 @@
 Msg   := 'Build with the following configuration:'
+One   := '1. make -f windows-universal.mk'
 
 all:
 	@echo -e $(Msg)
+	@echo -e $(One)    "\n\tThe default Universal Windows platform build"
