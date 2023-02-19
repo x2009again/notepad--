d5d4683d3fddd6f6b0e9a633e2c921db50d46240
zinface <zinface@163.com>
2023-02-19 22:44:27 +0800

    repo: 为 独立 Linux 平台的 Uos 构建进行打包时检查 OS ID 是否为 Uos

--- linux-uos.mk
+++ linux-uos.mk
@@ -37,3 +37,15 @@ package-contents-tree:
 	-tree $(builddir)/_CPack_Packages/Linux/DEB/
 
 # 此配置为构建 linux 通用版本构建
+
+# 一次系统检察
+UOS_OS_ID=$(shell lsb_release -si)
+ifneq ($(UOS_OS_ID),Uos)
+linux-uos:
+	@echo "此目标不应该由 $(UOS_OS_ID) 来构建 Uos 配方, 否则实际 Uos 系统可能由于 Qt 版本过低将无法使用."
+linux-uos-release:
+	@echo "此目标不应该由 $(UOS_OS_ID) 来构建 Uos 配方, 否则实际 Uos 系统可能由于 Qt 版本过低将无法使用."
+package:
+	@echo "此目标不应该由 $(UOS_OS_ID) 来构建 Uos 配方, 否则实际 Uos 系统可能由于 Qt 版本过低将无法使用."
+endif
+# repo: 要求使用 Uos 平台来进行独立 Linux 平台的 Uos 构建
