04d866ea89900d19c29d7013b03521d404c08639
zinface <zinface@163.com>
2023-03-22 23:01:49 +0800

    cmake-plugin: 处理在 Windows 中基于源代码构建的插件显示乱码问题
    1. 插件说明使用 u8 来确保 QString 的正常值显示
    2. 使用 UTF-8 with BOM  来确保源代码文件不会继承报错

--- src/plugin/versionUpdate/plugin.cpp
+++ src/plugin/versionUpdate/plugin.cpp
@@ -1,4 +1,4 @@
-#include <qobject.h>
+﻿#include <qobject.h>
 #include <qstring.h>
 #include <include/pluginGl.h>
 #include <functional>
@@ -7,5 +7,5 @@
 
 
 
-NOTEPAD_PLUGIN_METADATA_IDENTIFY("检查更新", "v0.1", "zinface", "Notepad-- 版本检查", "");
+NOTEPAD_PLUGIN_METADATA_IDENTIFY(u8"检查更新", "v0.1", "zinface", u8"Notepad-- 版本检查", "");
 NOTEPAD_PLUGIN_METADATA_IMPLEMENT(NddPluginImplement, false);
