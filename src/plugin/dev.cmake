f232da29c7c456b2760b16961838b9f4e5ff8f16
zinface <zinface@163.com>
2024-03-23 16:20:48 +0800

    cmake-plugin: 插件框架的基础模式固化，并提供框架开发者计划方案 dev.cmake

---
+++ src/plugin/dev.cmake
@@ -0,0 +1,15 @@
+# DO NOT COMMIT THIS FILE!
+
+# [开发者计划](Developer plan)：
+    # 不改变 CMakeLists.txt 的情况下开发自己的插件(Develop your own plugin without changing CMakeLists.txt)
+    
+# [I] : copy the provided examples. like this:
+# 1. 基于 git 仓库在线构建 helloworld 插件(Build the helloworld plug-in online based on the git repository.)
+# add_framework_plugin_with_git(
+#     https://gitee.com/zinface/notepad--plugin.helloworld
+#     --branch=cmake-plugins-dev)
+
+# Now, develop your new plugin:
+
+
+
